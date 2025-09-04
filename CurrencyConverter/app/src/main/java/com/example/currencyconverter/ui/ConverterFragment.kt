40b5bdf572471393c0880697e8b07373544c4bda
Ahmed Gamal <ahred33378797@gmail.com>
2025-09-04 22:43:55 +0300

    first ui screen converter screen

---
+++ CurrencyConverter/app/src/main/java/com/example/currencyconverter/ui/ConverterFragment.kt
@@ -0,0 +1,38 @@
+package com.example.currencyconverter.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.currencyconverter.R
+import com.example.currencyconverter.databinding.FragmentConverterBinding
+
+
+class ConverterFragment : Fragment() {
+
+    private var _binding:FragmentConverterBinding? =null
+    val binding:FragmentConverterBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentConverterBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding= null
+    }
+
+
+}
