5a19dcb517cd6a204b3a6e71e2909522b236a718
Ahmed Gamal <ahred33378797@gmail.com>
2025-09-05 20:32:14 +0300

    add get amount of currency conversion api call

---
+++ CurrencyConverter/app/src/main/java/com/example/currencyconverter/data/model/CurrencyConversionResponse.kt
@@ -0,0 +1,28 @@
+package com.example.currencyconverter.data.model
+
+import com.example.currencyconverter.domain.model.CurrencyConversionDto
+import com.google.gson.annotations.SerializedName
+
+data class CurrencyConversionResponse(
+
+	@field:SerializedName("date")
+	val date: String? = null,
+
+	@field:SerializedName("success")
+	val success: Boolean? = null,
+
+	@field:SerializedName("rates")
+	val rates: Map<String,Double>? = null,
+
+	@field:SerializedName("timestamp")
+	val timestamp: Int? = null,
+
+	@field:SerializedName("base")
+	val base: String? = null
+){
+	fun toCurrencyConversionDto():CurrencyConversionDto{
+		return CurrencyConversionDto(
+			conversionAmount = rates
+		)
+	}
+}
--- CurrencyConverter/app/src/main/java/com/example/currencyconverter/domain/contract/repo/RepoDataSoruce.kt
+++ CurrencyConverter/app/src/main/java/com/example/currencyconverter/domain/contract/repo/RepoDataSoruce.kt
@@ -1,10 +1,16 @@
 package com.example.currencyconverter.domain.contract.repo
 
 import com.example.currencyconverter.data.utils.ApiResult
+import com.example.currencyconverter.domain.model.CurrencyConversionDto
 import com.example.currencyconverter.domain.model.CurrencyDto
 import kotlinx.coroutines.flow.Flow
 
 interface RepoDataSoruce {
 
     suspend fun getAllCurrencies():Flow<ApiResult<CurrencyDto>>
+
+    suspend fun getCurrencyConversion(
+        base:String,
+        symbol:String
+    ):Flow<ApiResult<CurrencyConversionDto>>
 }
